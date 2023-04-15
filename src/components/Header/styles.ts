7af3ec5f369ee6b23bce5c5802e5fc6d0a11c926
Tiotedd <andrelsbrito2022@gmail.com>
2023-04-15 19:18:22 -0300

    first commit

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,7 @@
+import { StyleSheet } from "react-native/types"
+
+export const styles = StyleSheet.create({
+  container: {
+
+  }
+});
---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,8 @@
+import { styles } from "./styles";
+import { Header } from "../../components/Header";
+
+export function Home(){
+  return(
+    <Header/>
+  )
+}
